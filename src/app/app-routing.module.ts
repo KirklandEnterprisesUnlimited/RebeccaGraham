1a701947817f7bd6fe51b1700b7ece47361a2807
Stephen Kirkland <skirkland.job@gmail.com>
2021-08-19 03:01:57 -0400

    Basic for layout for my personal website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
