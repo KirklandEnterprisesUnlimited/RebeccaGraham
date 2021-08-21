84a1d40aa789b840f161266bba8c58e530fa3712
Stephen Kirkland <skirkland.job@gmail.com>
2021-08-21 03:32:33 -0400

    Good changes so far

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -5,7 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class ImageService {
 
-  public flamingo: string = "assets/images/people/profile.jpg";
+  public flamingo: string = "assets/images/people/flamingo.jpg";
+  public profile: string = "assets/images/people/profile.jpg";
 
   constructor() { }
 }
